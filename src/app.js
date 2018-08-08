bad282b948b8515849a20a47090577fff7632016
leeburgess67 <leeburgessmail@gmail.com>
2018-08-08 12:52:20 +0100

    requires classes for app.js:

--- src/app.js
+++ src/app.js
@@ -1,3 +1,12 @@
+const Countries = require('./models/countries.js');
+const SelectView = require('./views/select_view.js');
+const ResultView = require('./views/result_view.js');
+
+
 document.addEventListener('DOMContentLoaded', () => {
   console.log('JavaScript Loaded');
+
+  const countries = new Countries();
+  countries.getData();
+  
 });
